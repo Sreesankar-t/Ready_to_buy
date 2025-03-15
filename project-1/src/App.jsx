86c1445d388404640d3e4e26fb62acc7133bcdac
Sreesankar-t <sreeshankerthekkan@gmail.com>
2025-03-15 21:59:58 +0530

    second commit

---
+++ project-1/src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from '@mui/material';
+
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <div>
+                <Button variant="outlined" color="primary">
+                    Secondary
+                </Button>
+            </div>
+        </>
+    );
+}
+
+export default App;
