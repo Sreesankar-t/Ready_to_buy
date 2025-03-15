14a26a8774ac36e8461e4a0acebff3f4651680eb
Sreesankar-t <sreeshankerthekkan@gmail.com>
2025-03-15 22:22:59 +0530

    theme setings

--- project-1/src/main.jsx
+++ project-1/src/main.jsx
@@ -4,7 +4,19 @@ import { createTheme, ThemeProvider } from '@mui/material';
 import './index.css';
 import App from './App.jsx';
 
-const theme = createTheme({});
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#007bff',
+        },
+        secondary: {
+            main: '#ff5722',
+        },
+    },
+    typography: {
+        fontFamily: 'Arial, sans-serif',
+    },
+});
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
